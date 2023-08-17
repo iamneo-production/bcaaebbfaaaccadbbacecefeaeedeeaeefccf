b9ddbae4a99ed147cb535283ac48c83324b80292
Iamneo <techadmin@iamneo.ai>
2023-08-17 09:38:43 +0000

    Backup Commit

---
+++ target/aishu/src/main/java/com/jeon/aishu/AishuApplication.java
@@ -0,0 +1,13 @@
+package com.jeon.aishu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AishuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AishuApplication.class, args);
+	}
+
+}
---
+++ target/aishu/src/test/java/com/jeon/aishu/AishuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeon.aishu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AishuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
